241a9bcb1f1ea9f1020531065f1fc344971ff6db
ejiahug <ejiahug@E7B499BAE2EAA1.ericsson.se>
2014-10-16 16:33:59 +0800

    Add the support of the notification of TO-DO items # in the right top corner.

--- src/com/cgc/mobileappsig/eleave/MainTabActivity.java
+++ src/com/cgc/mobileappsig/eleave/MainTabActivity.java
@@ -10,14 +10,17 @@ import android.app.TabActivity;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 import android.content.Intent;
+import android.widget.RadioButton;
 import android.widget.TabHost;
 import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.Button;
+import android.widget.TabWidget;
 import android.view.View.OnClickListener;
 import android.view.View;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 
+import com.readystatesoftware.viewbadger.BadgeView;
 
 import com.cgc.mobileappsig.eleave.common.ExitApplication;
 
@@ -30,6 +33,9 @@ public class MainTabActivity extends TabActivity implements OnCheckedChangeListe
 	private Intent iReport;
 	private Intent iHelp;
 	private Button iexit;
+	private RadioButton btn_help;
+	private BadgeView badge;
+	private Integer todo_num;
 	
 	@Override  
     protected void onCreate(Bundle savedInstanceState) {  
@@ -52,9 +58,42 @@ public class MainTabActivity extends TabActivity implements OnCheckedChangeListe
 	        	.setIndicator(getResources().getString(R.string.main_apply), getResources().getDrawable(R.drawable.icon_2_n))
 	        	.setContent(iApply));
 		
+		iReport = new Intent(this, HomeActivity.class);
+			tabhost.addTab(tabhost.newTabSpec("iReport")
+		        	.setIndicator(getResources().getString(R.string.main_report), getResources().getDrawable(R.drawable.icon_3_n))
+		        	.setContent(iReport));
+		
+		iHelp = new Intent(this, HomeActivity.class);
+			tabhost.addTab(tabhost.newTabSpec("iHelp")
+		        	.setIndicator(getResources().getString(R.string.main_help), getResources().getDrawable(R.drawable.icon_3_n))
+		        	.setContent(iHelp));
+		
 		iexit = (Button)findViewById(R.id.radio_exit);
 		iexit.setOnClickListener(exitClick);
 		ExitApplication.getInstance().addActivity(this);
+		
+		btn_help = (RadioButton)findViewById(R.id.radio_help);
+		btn_help.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				mainTab.check(R.id.radio_help);
+				badge.hide();
+			}
+			
+		}
+		);
+		
+		badge = new BadgeView(this, btn_help);
+		
+		// TO-DO: get the number of To-do items
+		todo_num = 3;
+		if (todo_num > 0){
+			badge.setText(todo_num.toString());
+			badge.show();
+		}
+		
 	}
 	
 	OnClickListener exitClick=new OnClickListener() {
